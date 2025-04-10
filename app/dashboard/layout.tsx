69d98f493bdae2b500d8ba0d3aba29322c32f8f5
SouhaiBusiness <i79916475@gmail.com>
2025-04-10 14:45:57 +0100

    favicon

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,17 @@ import { auth } from "@clerk/nextjs/server"
 import DashboardSidebar from "@/components/dashboard-sidebar"
 import { NotificationProvider } from "@/components/notification"
 
+
+export const metadata = {
+  title: "ETUDESFRANÇAISES - Ressources pour les étudiants de français",
+  description: "Une plateforme destinée aux étudiants en études de français pour accéder aux cours, quiz et examens.",
+  icons: {
+    icon: "/favicon.png", // or favicon.png if you want to use PNG
+    shortcut: "/favicon.png",
+  },
+}
+
+
 export default async function DashboardLayout({
   children,
 }: {
