f65642751b08fa5f94c79762203b12348e83281d
SouhaiBusiness <i79916475@gmail.com>
2025-04-25 12:28:04 +0100

    fix script google analytics placement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,62 +1,61 @@
-import type { ReactNode } from "react"
-import { ClerkProvider } from "@clerk/nextjs"
-import { Inter } from "next/font/google"
-import Navbar from "@/components/navbar"
-import Footer from "@/components/footer"
-import "./globals.css"
+import type { ReactNode } from 'react';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
+import './globals.css';
 import Aos from '@/components/Aos';
-import ScrollToTopBtn from "@/components/ScrollToTopBtn"
-import { NotificationProvider } from "@/components/notification"
-import NavigationEvents from "@/components/navigation-events" 
-import Script from "next/script" 
- 
+import ScrollToTopBtn from '@/components/ScrollToTopBtn';
+import { NotificationProvider } from '@/components/notification';
+import NavigationEvents from '@/components/navigation-events';
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "ETUDESFRANÇAISES - Ressources pour les étudiants de français",
-  description: "Une plateforme destinée aux étudiants en études de français pour accéder aux cours, quiz et examens.",
+  title: 'ETUDESFRANÇAISES - Ressources pour les étudiants de français',
+  description:
+    'Une plateforme destinée aux étudiants en études de français pour accéder aux cours, quiz et examens.',
   icons: {
-    icon: "/favicon.png", // or favicon.png if you want to use PNG
-    shortcut: "/favicon.png",
+    icon: '/favicon.png', // or favicon.png if you want to use PNG
+    shortcut: '/favicon.png',
   },
-}
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-   return (
+  return (
     <ClerkProvider>
-      <html lang="fr">
-        
-
+      <html lang='fr'>
+      <head>
+          {/* Google Analytics Script */}
+          <Script
+            strategy='afterInteractive'
+            src='https://www.googletagmanager.com/gtag/js?id=G-HPZ9LB6YP3'
+          />
+          <Script id='google-analytics' strategy='afterInteractive'>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HPZ9LB6YP3');
+            `}
+          </Script>
+        </head>
         <body className={`${inter.className} min-h-screen flex flex-col`}>
-       
-          {/*  Google Analytics Scripts */}
-          <Script 
-  strategy="afterInteractive" 
-  src="https://www.googletagmanager.com/gtag/js?id=G-HPZ9LB6YP3"
-/>
-<Script id="google-analytics" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-HPZ9LB6YP3');
-  `}
-</Script>
+           
 
-        <NotificationProvider>
-          <Navbar />
-          <Aos>
-          <main className="flex-grow">{children}</main>
-          </Aos>
-      <ScrollToTopBtn />
-          <Footer />
-          {/*  Navigation Events for Google analytics */}
-          <NavigationEvents />
+          <NotificationProvider>
+            <Navbar />
+            <Aos>
+              <main className='flex-grow'>{children}</main>
+            </Aos>
+            <ScrollToTopBtn />
+            <Footer />
+            {/*  Navigation Events for Google analytics */}
+            <NavigationEvents />
           </NotificationProvider>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
-
