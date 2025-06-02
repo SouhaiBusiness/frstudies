567c5bf27a9fe378ea6a636316a0da09b89e1aaf
SouhaiBusiness <i79916475@gmail.com>
2025-06-02 23:46:10 +0100

    back to initial version of next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
 // next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://decent-spaniel-16.clerk.accounts.dev;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https://frstudies.vercel.app;
-              font-src 'self';
-              connect-src 'self' https://frstudies.vercel.app https://decent-spaniel-16.clerk.accounts.dev;
-              frame-src 'self' https://clerk.vercel.app https://decent-spaniel-16.clerk.accounts.dev;
-            `.replace(/\s+/g, ' ').trim()
-          },
-          // ... rest of your headers
-        ]
-      }
-    ];
-  },
-  // ... rest of your config
+  // Your basic Next.js configuration options go here
+  reactStrictMode: true,
 };
