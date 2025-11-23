472dce906a3329ca4e3f76bcd5868adcb0c45533
SouhaiBusiness <i79916475@gmail.com>
2025-11-23 03:03:12 +0100

    protect dashboard in rxt and remove icon 16*16 version

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,12 +100,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             sizes="32x32"
             href="/favicon-32x32.png"
           />
-          <link
+         {/* <link
             rel="icon"
             type="image/png"
             sizes="16x16"
             href="/favicon-16x16.png"
-          />
+          />*/}
           <link
             rel="apple-touch-icon"
             sizes="180x180"
